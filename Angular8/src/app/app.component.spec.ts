95fc0f7a75721fd198107898c71081c79aa2e58c
GUSTH01 <gustavofonsecasantos108@gmail.com>
2025-05-26 10:11:03 -0300

    Alteração de arquivos

--- Angular8/src/app/app.component.spec.ts
+++ Angular8/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Angular8'`, () => {
+  it(`should have as title 'projteste'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('Angular8');
+    expect(app.title).toEqual('projteste');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('Angular8 app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('projteste app is running!');
   });
 });
